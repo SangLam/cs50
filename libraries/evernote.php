9034bd0593badaea3bb40243d4dd57be9580a3f1
Sang Fui Lam <sang.fui.lam+github@gmail.com>
2016-09-01 23:42:27 +0100

    add search by tags functionality

--- libraries/evernote.php
+++ libraries/evernote.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /*hold the class interface for interacting with evernote*/
 
 class Evernote {
@@ -8,7 +8,7 @@ class Evernote {
 	private static $china;
 	private static $client;
 
-	public static function init($token, $sandbox, $china = false) 
+	public static function init($token, $sandbox, $china = false)
 	{
 		self::$token = $token;
 		self::$sandbox = $sandbox;
@@ -21,24 +21,32 @@ class Evernote {
 		return self::$client;
 	}
 
-	private static function getTaskNotebookGuid() 
-	{			
+	private static function getTaskNotebookGuid()
+	{
 		return 'b7fc2f7e-cd1d-46b1-8755-5062af555b71';
 	}
 
-	private static function getTaskNotes($filter) 
+	private static function getTaskNotes($filter)
 	{
 		$notesMetadataResultSpec = new \EDAM\NoteStore\NotesMetadataResultSpec([
-			'includeTitle' => true, 
+			'includeTitle' => true,
 			'includeUpdated' => true,
-			'includeTagGuids' => true				
+			'includeTagGuids' => true
 		]);
 		$offset = 0;
 		$maxResult = 250;
-		return self::getClient()->getUserNoteStore()->findNotesMetadata($filter, $offset, $maxResult, $notesMetadataResultSpec); 
+		try
+		{
+			return self::getClient()->getUserNoteStore()->findNotesMetadata($filter, $offset, $maxResult, $notesMetadataResultSpec);
+		}
+		catch(Exception $e)
+		{
+			echo 'Message: ' . $e->getMessage();
+			exit;
+		}
 	}
 
-	public static function getTitle($notes) 
+	public static function getTitle($notes)
 	{
 		$titles = array();
 		for ($i = 0; $i < $notes->totalNotes; $i++) {
@@ -47,7 +55,7 @@ class Evernote {
 		return $titles;
 	}
 
-	public static function getAllTaskNotes() 
+	public static function getAllTaskNotes()
 	{
 		$filter = new \EDAM\NoteStore\NoteFilter(['notebookGuid' => self::getTaskNotebookGuid()]);
 
@@ -56,20 +64,51 @@ class Evernote {
 		return $notes;
 	}
 
-	public static function getTaskNotesWithTerm($term) 
-	{			
-		$filter = new \EDAM\NoteStore\NoteFilter(['notebookGuid' => self::getTaskNotebookGuid(), 'words' => $term]);
+	public static function getTaskNotesWithTerm($term)
+	{
+		$$filter = new \EDAM\NoteStore\NoteFilter(['notebookGuid' => self::getTaskNotebookGuid(), 'words' => $term]);
 
 		$notes = self::getTaskNotes($filter);
 
 		return $notes;
 	}
 
-	public static function getTaskNotesWithTag($tags) 
+	public static function getTaskNotesWithTag($tags)
 	{
+		$filter = new \EDAM\NoteStore\NoteFilter(['notebookGuid' => self::getTaskNotebookGuid(), 'tagGuids' => $tags]);
+		print_r($filter);flush();
 
+		$notes = self::getTaskNotes($filter);
+
+		return $notes;
 	}
 
-	private static function hasMedia($content) {}
+	private static function hasMedia($content)
+	{
+	}
+
+	/*********************************************************
+	 *
+	 * Helper functions for developemnt
+	 *
+	 ********************************************************/
+
+	public static function getTagGuid()
+	{
+		$notes = self::getAllTaskNotes();
+		$tags = array();
+		foreach ($notes->notes as $note)
+		{
+			if(isset($note->tagGuids))
+			{
+				foreach($note->tagGuids as $id)
+				{
+					array_push($tags, $id);
+				}
+			}
+		}
+		return $tags;
+	}
+
 }
 ?>
